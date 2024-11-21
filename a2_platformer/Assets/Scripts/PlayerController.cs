574f72c79437fb65df4b20f8032130ce40184f8c
SideSwipeJoshy2 <hargobij@sheridancollege.ca>
2024-11-20 22:11:27 -0500

    journal week10

--- a2_platformer/Assets/Scripts/PlayerController.cs
+++ a2_platformer/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,11 +9,22 @@ public class PlayerController : MonoBehaviour
     {
         left, right
     }
+    public float timeToReachMaxSpeed;
+    public float speed;
+    public float timeToDeaccel;
 
+
+    private float accel;
+    private float deaccel;
+
+    public FacingDirection direction;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        accel = speed / timeToReachMaxSpeed;
+        deaccel = speed / timeToDeaccel;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,24 +34,68 @@ public class PlayerController : MonoBehaviour
         //manage the actual movement of the character.
         Vector2 playerInput = new Vector2();
         MovementUpdate(playerInput);
+        GetFacingDirection();
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        Vector2 currentVelo = rb.velocity;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            currentVelo += accel * Vector2.right * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            currentVelo += accel * Vector2.left * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            currentVelo += accel * Vector2.up * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            currentVelo += accel * Vector2.down * Time.deltaTime;
+        }
+
+        rb.velocity = currentVelo;
 
     }
 
-    public bool IsWalking()
+        public bool IsWalking()
     {
-        return false;
+        if (speed > 0)
+        {
+          return true;
+        }
+        else
+        {
+     return false;
+     }
     }
     public bool IsGrounded()
     {
-        return false;
+        return true;
     }
 
     public FacingDirection GetFacingDirection()
     {
-        return FacingDirection.left;
+
+
+        if(direction == FacingDirection.left)
+        {
+            return FacingDirection.left;
+        }
+        else if(direction == FacingDirection.right)
+        {
+            return FacingDirection.right;
+          
+        }
+        return direction;
     }
+
+
 }
